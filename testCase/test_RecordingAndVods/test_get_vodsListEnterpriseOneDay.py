4baac271da9fddbd0c5477c053128e8db121b78b
tiantian <1192923936@qq.com>
2020-08-04 16:36:59 +0800

    test_get_vodsListEnterpriseOneDay submit

--- testCase/test_RecordingAndVods/test_get_vodsListEnterpriseOneDay.py
+++ testCase/test_RecordingAndVods/test_get_vodsListEnterpriseOneDay.py
@@ -30,11 +30,11 @@ class Test_get_vodsListEnterpriseOneDay:
                                                                       self.token)
 
         # 获取接口的基本base_url
-        self.get_vodsList_accordDeviceNum_base_url = get_parameter.get_parameter().get_baseUrl(
-            ExcelName='recordingVods_casedata.xlsx', sheetName='base_url', apiName='get_vodsList_accordDeviceNum')
-        # print('get_vodsList_accordDeviceNum_base_url is: ', self.get_vodsList_accordDeviceNum_base_url)
+        self.get_vodsList_enterpriseOneDay_base_url = get_parameter.get_parameter().get_baseUrl(
+            ExcelName='recordingVods_casedata.xlsx', sheetName='base_url', apiName='get_vodsList_enterpriseOneDay')
+        # print('get_vodsList_enterpriseOneDay_base_url is: ', self.get_vodsList_enterpriseOneDay_base_url)
 
-        self.log.debug('get_vodsList_accordDeviceNum_base_url is: ' + self.get_vodsList_accordDeviceNum_base_url)
+        self.log.debug('get_vodsList_enterpriseOneDay_base_url is: ' + self.get_vodsList_enterpriseOneDay_base_url)
 
         # 实例化获取case data对象
         self.get_caseDataObj = get_testCaseData.Get_caseData()
@@ -44,3 +44,54 @@ class Test_get_vodsListEnterpriseOneDay:
 
         # 实例化writeExcel对象
         self.writeExcelObj = writeExcel.writeExcel('testCase', 'casedata', 'recordingVods_casedata.xlsx')
+
+
+    def test_get_vodsListEccordDeviceNum001(self):
+        '''
+        function:分页查询企业一天内的视频列表
+        :return:
+        '''
+
+        # 获取接口请求输入数据
+        startTime = self.get_caseDataObj.get_data(ExcelName='recordingVods_casedata.xlsx',
+                                                  sheetName='test_data',
+                                                  testName='test_get_vodsListEccordDeviceNum001', getdata='startTime')
+
+        endTime = self.get_caseDataObj.get_data(ExcelName='recordingVods_casedata.xlsx',
+                                                sheetName='test_data',
+                                                testName='test_get_vodsListEccordDeviceNum001', getdata='endTime')
+
+
+        pageIndex = self.get_caseDataObj.get_data(ExcelName='recordingVods_casedata.xlsx',
+                                                  sheetName='test_data',
+                                                  testName='test_get_vodsListEccordDeviceNum001', getdata='pageIndex')
+
+        pageSize = self.get_caseDataObj.get_data(ExcelName='recordingVods_casedata.xlsx',
+                                                 sheetName='test_data',
+                                                 testName='test_get_vodsListEccordDeviceNum001', getdata='pageSize')
+
+        # 获取期望返回码excepectCode
+        excepectCode = self.get_caseDataObj.get_data(ExcelName='recordingVods_casedata.xlsx',
+                                                     sheetName='ExpectedResult',
+                                                     testName='test_get_vodsListEccordDeviceNum001',
+                                                     getdata='expected_code')
+
+        # 获取期望返回码message
+        excepectMessage = self.get_caseDataObj.get_data(ExcelName='recordingVods_casedata.xlsx',
+                                                        sheetName='ExpectedResult',
+                                                        testName='test_get_vodsListEccordDeviceNum001',
+                                                        getdata='excepect_message')
+
+        print('startTime is: ', startTime)
+        print('endTime is: ', endTime)
+        print('pageIndex is: ', pageIndex)
+        print('pageSize is: ', pageSize)
+
+        print('excepectCode is: ', excepectCode)
+        print('excepectMessage is: ', excepectMessage)
+
+        code, body = self.recordingVodsObj.get_vodsList_enterpriseOneDay(self.get_vodsList_enterpriseOneDay_base_url,
+                                                                        startTime=startTime, endTime=endTime, pageIndex=pageIndex,
+                                                                        pageSize=pageSize)
+        print('code is: ', code)
+        print('body is: ', body)
